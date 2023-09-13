86fd31d770d42a2cd4c5e2759b5ae856b58ad2db
Thiago Baratieri <thiagobaratieri971@gmail.com>
2023-09-13 02:51:43 -0300

    pagina do banco de dados criada

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -6,7 +6,14 @@ def home(request):
 
 
 def usuarios(request):
+    # Salvar dados da tela para o banco de dados
     novo_usuario = Usuario()
     novo_usuario.nome = request.POST.get('nome')
     novo_usuario.idade = request.POST.get('idade')
     novo_usuario.save()
+    # Exibir dados em uma nova página
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    # Retornar os dados para a página
+    return render(request,'usuarios/usuarios.html',usuarios)
