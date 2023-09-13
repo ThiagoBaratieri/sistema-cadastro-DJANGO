c281c88898f63d1a1f09e64593e545a5e5041a66
Thiago Baratieri <thiagobaratieri971@gmail.com>
2023-09-13 02:38:22 -0300

    criação e config do banco de dados

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Usuario
 
 def home(request):
     return render(request, 'usuarios/home.html')
+
+
+def usuarios(request):
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
